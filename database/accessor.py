822dea91aa7fb819392716c5ed418804f027229a
Никита Кузнецов <eudaimonia22@yandex.ru>
2025-01-17 22:33:06 +0700

    some error

--- database/accessor.py
+++ database/accessor.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import sessionmaker
 from settings import settings
 
 
-engine = create_engine(settings.db_url)
+# engine = create_engine(settings.db_url)
+
+
+engine = create_engine(
+    f'postgresql+psycopg2://{settings.POSTGRES_USER}:'
+    f'{settings.POSTGRES_PASSWORD}@localhost:'
+    f'{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}'
+)
 
 
 Session = sessionmaker(engine)
--- settings.py
+++ settings.py
@@ -2,6 +2,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    POSTGRES_DRIVER: str = 'postgresql+psycopg2'
+    POSTGRES_PASSWORD: str = 'mysecretpassword'
+    POSTGRES_USER: str = 'postgres_user'
+    POSTGRES_DB: str = 'pomodoro'
+    POSTGRES_HOST: str = 'localhost'
+    POSTGRES_PORT: int = 5432
+
     DB_DRIVER: str = 'postgresql+psycopg2'
     DB_PASSWORD: str = 'mysecretpassword'
     DB_USER: str = 'postgres_user'
@@ -17,6 +24,24 @@ class Settings(BaseSettings):
     GOOGLE_CLIENT_SECRET: str = ''
     GOOGLE_REDIRECT_URI: str = ''
     GOOGLE_TOKEN_URL: str = 'https://accounts.google.com/o/oauth2/token'
+
+
+# class Settings(BaseSettings):
+#     DB_DRIVER: str = 'postgresql+psycopg2'
+#     DB_PASSWORD: str = 'mysecretpassword'
+#     DB_USER: str = 'postgres_user'
+#     DB_NAME: str = 'pomodoro'
+#     DB_HOST: str = 'localhost'
+#     DB_PORT: int = 5432
+#     CACHE_HOST: str = '127.0.0.1'
+#     CACHE_PORT: int = 6379
+#     CACHE_DB: int = 0
+#     JWT_SECRET_KEY: str = 'secret_key'
+#     JWT_ENCODE_ALHORITHM: str = 'HS256'
+#     GOOGLE_CLIENT_ID: str = ''
+#     GOOGLE_CLIENT_SECRET: str = ''
+#     GOOGLE_REDIRECT_URI: str = ''
+#     GOOGLE_TOKEN_URL: str = 'https://accounts.google.com/o/oauth2/token'
 
 
     @property
