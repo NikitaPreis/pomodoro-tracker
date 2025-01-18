6a1c9acffe0dff48377218e2cb9dfd1cca821cbe
Никита Кузнецов <eudaimonia22@yandex.ru>
2025-01-18 20:31:41 +0700

    useless commit

--- database/accessor.py
+++ database/accessor.py
@@ -4,14 +4,15 @@ from sqlalchemy.orm import sessionmaker
 from settings import settings
 
 
-# engine = create_engine(settings.db_url)
+engine = create_engine(settings.db_url)
 
+engine = create_engine('postgresql+psycopg2://postgres_user:mysecretpassword@localhost:5432/pomodoro')
 
-engine = create_engine(
-    f'postgresql+psycopg2://{settings.POSTGRES_USER}:'
-    f'{settings.POSTGRES_PASSWORD}@localhost:'
-    f'{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}'
-)
+# engine = create_engine(
+#     f'postgresql+psycopg2://{settings.POSTGRES_USER}:'
+#     f'{settings.POSTGRES_PASSWORD}@localhost:'
+#     f'{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}'
+# )
 
 
 Session = sessionmaker(engine)
